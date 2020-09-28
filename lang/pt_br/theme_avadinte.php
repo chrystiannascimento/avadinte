6b8c13b32f8563e7fd33c58c5bc668c4e9555457
Chrystian Gustavo <chrystiang20@gmail.com>
2020-09-28 13:08:27 -0300

    adicionando pt_br

---
+++ lang/pt_br/theme_avadinte.php
@@ -0,0 +1,77 @@
+<?php
+
+/**
+ * Language file.
+ *
+ * @package   theme_avadinte
+ * @copyright 2020 
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+// This line protects the file from being accessed by a URL directly.
+defined('MOODLE_INTERNAL') || die();
+
+// The name of the second tab in the theme settings.
+$string['advancedsettings'] = 'Configurações avançadas';
+// The backgrounds tab name.
+$string['backgrounds'] = 'Backgrounds';
+// The brand colour setting.
+$string['brandcolor'] = 'Cor da Logo';
+// The brand colour setting description.
+$string['brandcolor_desc'] = 'A cor de destaque.';
+// A description shown in the admin theme selector.
+$string['choosereadme'] = 'Tema avadinte é um tema filho do Boost. ';
+// Name of the settings pages.
+$string['configtitle'] = 'Configurações do avadinte ';
+// Background image for dashboard page.
+$string['dashboardbackgroundimage'] = 'Imagem de fundo da página Painel';
+// Background image for dashboard page.
+$string['dashboardbackgroundimage_desc'] = 'Uma imagem que será esticada para preencher o fundo da página do painel.';
+// Background image for default page.
+$string['defaultbackgroundimage'] = 'Imagem de fundo da página padrão';
+// Background image for default page.
+$string['defaultbackgroundimage_desc'] = 'Uma imagem que será esticada para preencher o fundo de todas as páginas sem uma imagem de fundo mais específica.';
+// Background image for front page.
+$string['frontpagebackgroundimage'] = 'Imagem de fundo da página inicial';
+// Background image for front page.
+$string['frontpagebackgroundimage_desc'] = 'Uma imagem que será esticada para preencher o fundo da página inicial';
+// Name of the first settings tab.
+$string['generalsettings'] = 'Configurações gerais';
+// Background image for incourse page.
+$string['incoursebackgroundimage'] = 'Imagem de fundo da página do curso';
+// Background image for incourse page.
+$string['incoursebackgroundimage_desc'] = 'Uma imagem que será esticada para preencher o fundo das páginas do curso.';
+// Background image for login page.
+$string['loginbackgroundimage'] = 'Imagem de fundo da página de login';
+// Background image for login page.
+$string['loginbackgroundimage_desc'] = 'Uma imagem que será esticada para preencher o fundo da página de login.';
+// The name of our plugin.
+$string['pluginname'] = 'AVA DINTE';
+// Preset files setting.
+$string['presetfiles'] = 'Arquivos adicionais de predefinição de tema';
+// Preset files help text.
+$string['presetfiles_desc'] = 'Arquivos predefinidos podem ser usados ​​para alterar drasticamente a aparência do tema. Consulte <a href=https://docs.moodle.org/dev/Boost_Presets> Predefinições do Boost </a> para obter informações sobre como criar e compartilhar seus próprios arquivos predefinidos e consulte <a href = http: //moodle.net / boost> Repositório de predefinições </a> para predefinições que outros compartilharam.';
+// Preset setting.
+$string['preset'] = 'Tema predefinido';
+// Preset help text.
+$string['preset_desc'] = 'Escolha uma predefinição para alterar amplamente a aparência do tema..';
+// Raw SCSS setting.
+$string['rawscss'] = 'SCSS bruto';
+// Raw SCSS setting help text.
+$string['rawscss_desc'] = 'Use este campo para fornecer código SCSS ou CSS que será injetado no final da folha de estilo.';
+// Raw initial SCSS setting.
+$string['rawscsspre'] = 'SCSS inicial bruto';
+// Raw initial SCSS setting help text.
+$string['rawscsspre_desc'] = 'Neste campo você pode fornecer o código SCSS de inicialização, ele será injetado antes de tudo. Na maioria das vezes você usará esta configuração para definir variáveis.';
+// We need to include a lang string for each block region.
+$string['region-side-pre'] = 'Direito';
+$string['homepagetitle'] ='Página inicial';
+$string['coursespagetitle'] ='Cursos';
+$string['frontpage'] ='Página inicial';
+$string['mycoursespagetitle'] ='Meus cursos';
+$string['calendar'] = 'Calendário';
+$string['attendtitle'] ='Central de atendimento';
+$string['privatefilestitle'] = 'Arquivos privados';
+$string['contentbanktitle'] = 'Banco de conteúdo';
+$string['siteadmintitle'] = 'Administração do site';
+$string['region-frontpage'] = 'Blocos da página Inicial ';
