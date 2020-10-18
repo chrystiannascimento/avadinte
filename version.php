9788b9fb49ad87107c14f2e3f80105ccac46f243
unknown <chrystiang20@gmail.com>
2020-10-18 19:08:10 -0300

    2020101801

--- version.php
+++ version.php
@@ -5,7 +5,7 @@
 defined('MOODLE_INTERNAL') || die();                                                                                                
  
 // This is the version of the plugin.                                                                                               
-$plugin->version = 2020101800;                                                                                                    
+$plugin->version = 2020101801;                                                                                                    
  
 // This is the version of Moodle this plugin requires.                                                                              
 $plugin->requires = 2016070700;                                                                                                   
