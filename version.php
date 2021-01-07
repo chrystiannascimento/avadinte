af39f460680696d083b58e309caecf8b7f686641
chrystiannascimento <48285849+chrystiannascimento@users.noreply.github.com>
2021-01-07 12:00:49 -0300

    fix: scss 310

--- version.php
+++ version.php
@@ -5,7 +5,7 @@
 defined('MOODLE_INTERNAL') || die();                                                                                                
  
 // This is the version of the plugin.                                                                                               
-$plugin->version = 2020122101;                                                                                                    
+$plugin->version = 2020122300;                                                                                                    
  
 // This is the version of Moodle this plugin requires.                                                                              
 $plugin->requires = 2016070700;                                                                                                   
