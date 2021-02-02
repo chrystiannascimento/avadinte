8af3a9d87026d0679f8177c1cd18261ed815bbc0
chrystiannascimento <48285849+chrystiannascimento@users.noreply.github.com>
2021-02-02 10:40:09 -0300

    mudou

--- version.php
+++ version.php
@@ -5,7 +5,7 @@
 defined('MOODLE_INTERNAL') || die();                                                                                                
  
 // This is the version of the plugin.                                                                                               
-$plugin->version = 2021020201;                                                                                                    
+$plugin->version = 2021020209;                                                                                                    
  
 // This is the version of Moodle this plugin requires.                                                                              
 $plugin->requires = 2016070700;                                                                                                   
