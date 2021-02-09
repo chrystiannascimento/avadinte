0c6b785f0494eafe208355bac61f3f60c11dd87d
chrystiannascimento <48285849+chrystiannascimento@users.noreply.github.com>
2021-02-09 09:43:26 -0300

    fix: double scroll on admin page

--- version.php
+++ version.php
@@ -5,7 +5,7 @@
 defined('MOODLE_INTERNAL') || die();                                                                                                
  
 // This is the version of the plugin.                                                                                               
-$plugin->version = 2021020209;                                                                                                    
+$plugin->version = 2021020400;                                                                                                    
  
 // This is the version of Moodle this plugin requires.                                                                              
 $plugin->requires = 2016070700;                                                                                                   
